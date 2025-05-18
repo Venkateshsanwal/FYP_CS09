ae1367e63c5e1f5d0dd3c5c60ced9b9533d2f628
Venkatesh sanwal <115449810+Venkateshsanwal@users.noreply.github.com>
2025-05-18 17:18:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies (Render will usually handle this in the build step)
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 # Create output directory
 mkdir -p output
